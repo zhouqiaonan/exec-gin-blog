b8de16302f60ecb176701d574f01f956cb92faef
zhouqiaonan <zhouqiaonan@163.com>
2026-05-22 18:00:09 +0800

    feat:logger and cors

--- go.mod
+++ go.mod
@@ -22,13 +22,17 @@ require (
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible // indirect
+	github.com/lestrrat-go/strftime v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.22 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.3.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/quic-go/qpack v0.6.0 // indirect
 	github.com/quic-go/quic-go v0.59.1 // indirect
 	github.com/redis/go-redis/v9 v9.19.0 // indirect
+	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5 // indirect
 	github.com/sirupsen/logrus v1.9.4 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.3.1 // indirect
--- middleware/logger.go
+++ middleware/logger.go
@@ -3,8 +3,11 @@ package middleware
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	retalog "github.com/lestrrat-go/file-rotatelogs"
+	"github.com/rifflock/lfshook"
 	"github.com/sirupsen/logrus"
 )
 
@@ -19,4 +22,61 @@ func Logger() gin.HandlerFunc {
 	logger := logrus.New()
 	logger.Out = src
 	logger.SetLevel(logrus.DebugLevel)
+	logWriter, _ := retalog.New(
+		filePath+"%Y%m%d.log",
+		retalog.WithMaxAge(7*24*time.Hour),
+		retalog.WithRotationTime(7*24*time.Hour),
+		retalog.WithLinkName(linkName),
+	)
+	writeMap := lfshook.WriterMap{
+		logrus.InfoLevel:  logWriter,
+		logrus.FatalLevel: logWriter,
+		logrus.DebugLevel: logWriter,
+		logrus.WarnLevel:  logWriter,
+		logrus.ErrorLevel: logWriter,
+		logrus.PanicLevel: logWriter,
+	}
+	Hook := lfshook.NewHook(writeMap, &logrus.TextFormatter{
+		TimestampFormat: "2006-01-02 15:04:05",
+	})
+	logger.AddHook(Hook)
+	return func(c *gin.Context) {
+		startTime := time.Now()
+		c.Next()
+		stopTime := time.Since(startTime)
+		spendTime := fmt.Sprintf("%.2f", stopTime.Seconds())
+		hostName, err := os.Hostname()
+		if err != nil {
+			hostName = "unknown"
+		}
+		statusCode := c.Writer.Status()
+		clientIp := c.ClientIP()
+		method := c.Request.Method
+		path := c.Request.RequestURI
+		dataSize := c.Writer.Size()
+		if dataSize < 0 {
+			dataSize = 0
+		}
+		userAgent := c.Request.UserAgent()
+		entry := logger.WithFields(logrus.Fields{
+			"HostName":  hostName,
+			"status":    statusCode,
+			"SpendTime": spendTime,
+			"IP":        clientIp,
+			"Method":    method,
+			"Path":      path,
+			"DataSize":  dataSize,
+			"Agent":     userAgent,
+		})
+		if len(c.Errors) > 0 {
+			entry.Error(c.Errors.String())
+		}
+		if statusCode >= 500 {
+			entry.Error()
+		} else if statusCode >= 400 {
+			entry.Warn()
+		} else {
+			entry.Info()
+		}
+	}
 }
